e694fc4db18dfd966a69f4263d4177124e01811d
Tim Rudat <timrudat@gmail.com>
2014-12-27 05:32:42 +0100

    Improve JWT::DecodeError exception output

--- lib/jwt.rb
+++ lib/jwt.rb
@@ -26,19 +26,19 @@ module JWT
   def self.decode(token, secret_or_key = nil, verify = true, options = {}, &keyfinder)
     begin
       header, payload, signature = token.split('.')
-      
+
       header    = JSON.parse(Base64.urlsafe_decode64(header))
       payload   = JSON.parse(Base64.urlsafe_decode64(payload))
       signature = Base64.urlsafe_decode64(signature)
     rescue Exception => e
-      raise JWT::DecodeError
+      raise JWT::DecodeError.new e.message
     end
 
     valid = false
 
     if verify
       valid = signature === Base64.urlsafe_decode64(encode(payload, secret_or_key, header['alg'], header).split('.').last())
-      raise JWT::DecodeError unless valid
+      raise JWT::DecodeError.new('Token verification failed. Data corrupted or pass phrase incorrect.') unless valid
     end
 
     [header, payload, signature, valid]
--- spec/lib/jwt_spec.rb
+++ spec/lib/jwt_spec.rb
@@ -7,6 +7,7 @@ describe JWT do
   let(:jwt_payload) { { 'sub' => 1234567890, 'name' => 'John Doe', 'admin' => true } }
   let(:jwt_payload_base64) { 'eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKb2huIERvZSIsImFkbWluIjp0cnVlfQ==' }
   let(:secret) { 'secret' }
+  let(:wrong_secret) { 'wrong secret' }
   let(:jwt_signature_base64) { 'zzPVwrCDlyRQSEMsDCLrq4cjMl5t88H5T2msS_HgdqI=' }
   let(:token) { "#{jwt_header_base64}.#{jwt_payload_base64}.#{jwt_signature_base64}" }
 
@@ -19,12 +20,24 @@ describe JWT do
 
   context 'decode' do
     it 'should match given input data' do
+      expect { JWT.decode(token, secret) }.not_to raise_error
+
       header, payload, signature, valid = JWT.decode(token, secret)
 
       expect(header).to eq(jwt_header)
       expect(payload).to eq(jwt_payload)
       expect(signature).to eq(Base64.urlsafe_decode64(jwt_signature_base64))
       expect(valid).to eq(true)
+    end
+
+    context 'raises DecodeError' do
+      it 'if verification fails' do
+        expect { JWT.decode(token, wrong_secret) }.to raise_error(JWT::DecodeError)
+      end
+
+      it 'if input data is not valid' do
+        expect { JWT.decode([token, token].join, secret) }.to raise_error(JWT::DecodeError)
+      end
     end
   end
 end
