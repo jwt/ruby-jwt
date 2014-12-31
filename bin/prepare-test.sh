621aaa670576918b4ccd20d864cd1a87fbe036d0
Tim Rudat <timrudat@gmail.com>
2014-12-31 22:03:48 +0100

    Remove jwa folder from prepare-test script

--- bin/prepare-test.sh
+++ bin/prepare-test.sh
@@ -2,19 +2,19 @@
 
 mkdir -p tmp/certs/jwa
 
-openssl genrsa 2048 > tmp/certs/jwa/rsa-private.pem
-openssl genrsa 2048 > tmp/certs/jwa/rsa-wrong-private.pem
-openssl rsa -in tmp/certs/jwa/rsa-private.pem -pubout > tmp/certs/jwa/rsa-public.pem
-openssl rsa -in tmp/certs/jwa/rsa-wrong-private.pem -pubout > tmp/certs/jwa/rsa-wrong-public.pem
-openssl ecparam -out tmp/certs/jwa/ec256-private.pem -name secp256k1 -genkey
-openssl ecparam -out tmp/certs/jwa/ec256-wrong-private.pem -name secp256k1 -genkey
-openssl ecparam -out tmp/certs/jwa/ec384-private.pem -name secp384r1 -genkey
-openssl ecparam -out tmp/certs/jwa/ec384-wrong-private.pem -name secp384r1 -genkey
-openssl ecparam -out tmp/certs/jwa/ec512-private.pem -name secp521r1 -genkey
-openssl ecparam -out tmp/certs/jwa/ec512-wrong-private.pem -name secp521r1 -genkey
-openssl ec -in tmp/certs/jwa/ec256-private.pem -pubout > tmp/certs/jwa/ec256-public.pem
-openssl ec -in tmp/certs/jwa/ec256-wrong-private.pem -pubout > tmp/certs/jwa/ec256-wrong-public.pem
-openssl ec -in tmp/certs/jwa/ec384-private.pem -pubout > tmp/certs/jwa/ec384-public.pem
-openssl ec -in tmp/certs/jwa/ec384-wrong-private.pem -pubout > tmp/certs/jwa/ec384-wrong-public.pem
-openssl ec -in tmp/certs/jwa/ec512-private.pem -pubout > tmp/certs/jwa/ec512-public.pem
-openssl ec -in tmp/certs/jwa/ec512-wrong-private.pem -pubout > tmp/certs/jwa/ec512-wrong-public.pem
+openssl genrsa 2048 > tmp/certs/rsa-private.pem
+openssl genrsa 2048 > tmp/certs/rsa-wrong-private.pem
+openssl rsa -in tmp/certs/rsa-private.pem -pubout > tmp/certs/rsa-public.pem
+openssl rsa -in tmp/certs/rsa-wrong-private.pem -pubout > tmp/certs/rsa-wrong-public.pem
+openssl ecparam -out tmp/certs/ec256-private.pem -name secp256k1 -genkey
+openssl ecparam -out tmp/certs/ec256-wrong-private.pem -name secp256k1 -genkey
+openssl ecparam -out tmp/certs/ec384-private.pem -name secp384r1 -genkey
+openssl ecparam -out tmp/certs/ec384-wrong-private.pem -name secp384r1 -genkey
+openssl ecparam -out tmp/certs/ec512-private.pem -name secp521r1 -genkey
+openssl ecparam -out tmp/certs/ec512-wrong-private.pem -name secp521r1 -genkey
+openssl ec -in tmp/certs/ec256-private.pem -pubout > tmp/certs/ec256-public.pem
+openssl ec -in tmp/certs/ec256-wrong-private.pem -pubout > tmp/certs/ec256-wrong-public.pem
+openssl ec -in tmp/certs/ec384-private.pem -pubout > tmp/certs/ec384-public.pem
+openssl ec -in tmp/certs/ec384-wrong-private.pem -pubout > tmp/certs/ec384-wrong-public.pem
+openssl ec -in tmp/certs/ec512-private.pem -pubout > tmp/certs/ec512-public.pem
+openssl ec -in tmp/certs/ec512-wrong-private.pem -pubout > tmp/certs/ec512-wrong-public.pem
