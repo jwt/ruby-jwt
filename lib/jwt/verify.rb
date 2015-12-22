e556eb99cffaa8281746a1356f37850c716de7dc
Tim Rudat <timrudat@gmail.com>
2015-12-22 19:05:25 +0100

    Fix some code smells in JWT::Verify class

--- lib/jwt/verify.rb
+++ lib/jwt/verify.rb
@@ -1,23 +1,24 @@
 module JWT
+  # JWT verify methods
   module Verify
-    def self.verify_expiration payload, options
-      return if !payload.include?('exp')
+    def self.verify_expiration(payload, options)
+      return unless payload.include?('exp')
 
       if payload['exp'].to_i < (Time.now.to_i - options[:leeway])
         fail(JWT::ExpiredSignature, 'Signature has expired')
       end
     end
 
-    def self.verify_not_before payload, options
-      return if !payload.include?('nbf')
+    def self.verify_not_before(payload, options)
+      return unless payload.include?('nbf')
 
       if payload['nbf'].to_i > (Time.now.to_i + options[:leeway])
         fail(JWT::ImmatureSignature, 'Signature nbf has not been reached')
       end
     end
 
-    def self.verify_iss payload, options
-      return if !options[:iss]
+    def self.verify_iss(payload, options)
+      return unless options[:iss]
 
       if payload['iss'].to_s != options[:iss].to_s
         fail(
@@ -27,37 +28,36 @@ module JWT
       end
     end
 
-    def self.verify_iat payload, options
-      return if !payload.include?('iat')
+    def self.verify_iat(payload, options)
+      return unless payload.include?('iat')
 
       if !(payload['iat'].is_a?(Integer)) || payload['iat'].to_i > (Time.now.to_i + options[:leeway])
         fail(JWT::InvalidIatError, 'Invalid iat')
       end
     end
 
-    def self.verify_jti payload, options
+    def self.verify_jti(payload, options)
       fail(JWT::InvalidJtiError, 'Missing jti') if payload['jti'].to_s == ''
     end
 
-    def self.verify_aud payload, options
+    def self.verify_aud(payload, options)
       return if !options[:aud]
 
       if payload[:aud].is_a?(Array)
-        if !payload['aud'].include?(options[:aud].to_s)
-          fail(
-            JWT::InvalidAudError,
-            'Invalid audience'
-          )
-        end
+        fail(
+          JWT::InvalidAudError,
+          'Invalid audience'
+        ) unless payload['aud'].include?(options[:aud].to_s)
       else
-        if payload['aud'].to_s != options[:aud].to_s
-          fail(JWT::InvalidAudError, "Invalid audience. Expected #{options[:aud]}, received #{payload['aud'] || '<none>'}")
-        end
+        fail(
+          JWT::InvalidAudError,
+          "Invalid audience. Expected #{options[:aud]}, received #{payload['aud'] || '<none>'}"
+        ) unless payload['aud'].to_s == options[:aud].to_s
       end
     end
 
-    def self.verify_sub payload, options
-      return if !options[:sub]
+    def self.verify_sub(payload, options)
+      return unless options[:sub]
 
       if payload['sub'].to_s != options[:sub].to_s
         fail(
