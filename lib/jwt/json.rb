2d5bc862b88031b56a424a1558e84fafce9af422
Tim Rudat <timrudat@gmail.com>
2016-01-12 16:18:21 +0100

    Remove obsolete json code
    Remove obsolete code that supported older versions of ruby.

--- lib/jwt/json.rb
+++ lib/jwt/json.rb
@@ -1,31 +1,16 @@
+require 'json'
+
 module JWT
   # JSON fallback implementation or ruby 1.8.x
   module Json
-    if RUBY_VERSION >= '1.9' && !defined?(MultiJson)
-      require 'json'
+    def decode_json(encoded)
+      JSON.parse(encoded)
+    rescue JSON::ParserError
+      raise JWT::DecodeError, 'Invalid segment encoding'
+    end
 
-      def decode_json(encoded)
-        JSON.parse(encoded)
-      rescue JSON::ParserError
-        raise JWT::DecodeError, 'Invalid segment encoding'
-      end
-
-      def encode_json(raw)
-        JSON.generate(raw)
-      end
-
-    else
-      require 'multi_json'
-
-      def decode_json(encoded)
-        MultiJson.decode(encoded)
-      rescue MultiJson::LoadError
-        raise JWT::DecodeError, 'Invalid segment encoding'
-      end
-
-      def encode_json(raw)
-        MultiJson.encode(raw)
-      end
+    def encode_json(raw)
+      JSON.generate(raw)
     end
   end
 end
