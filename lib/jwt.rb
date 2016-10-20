2f837b259181fd0f14371e19a9d02f770b25ad07
Steve Sloan <steve@finagle.org>
2016-10-20 10:17:40 -0700

    Style changes from code review.

--- lib/jwt.rb
+++ lib/jwt.rb
@@ -140,8 +140,12 @@ module JWT
 
   def signature_algorithm_and_key(header, payload, key, &keyfinder)
     if keyfinder
-      key = (keyfinder.arity == 2) ? keyfinder.call(header, payload) : keyfinder.call(header)
-      raise JWT::DecodeError, 'No verification key available'  unless key
+      key = if keyfinder.arity == 2
+        yield(header, payload)
+      else
+        yield(header)
+      end
+      raise JWT::DecodeError, 'No verification key available' unless key
     end
     [header['alg'], key]
   end
