df087eebd10842d78f7670dc0114363086bf7741
A.B <a.b320012@gmail.com>
2017-09-07 09:03:05 -0400

    moved each sig strategy into seperate classes

---
+++ lib/jwt/algos/rsa.rb
@@ -0,0 +1,18 @@
+module JWT
+  module Algos
+    module Rsa
+      extend self
+      SUPPORTED = %w(RS256 RS384 RS512)
+
+      def sign to_sign
+        algorithm, msg, key = to_sign.values
+        raise EncodeError, "The given key is a #{key.class}. It has to be an OpenSSL::PKey::RSA instance." if key.class == String
+        key.sign(OpenSSL::Digest.new(algorithm.sub('RS', 'sha')), msg)
+      end
+
+      def verify to_verify
+        SecurityUtils.verify_rsa(to_verify.algorithm, to_verify.public_key, to_verify.signing_input, to_verify.signature)
+      end
+    end
+  end
+end
