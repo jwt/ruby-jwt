b94ad68c340bc3276a20419b92870050a21f4203
Joakim Antman <antmanj@gmail.com>
2020-10-25 22:20:15 +0200

    Support JWKs for pre 2.3 rubies

--- lib/jwt/jwk/rsa.rb
+++ lib/jwt/jwk/rsa.rb
@@ -54,7 +54,7 @@ module JWT
       end
 
       def encode_open_ssl_bn(key_part)
-        ::Base64.urlsafe_encode64(key_part.to_s(BINARY), padding: false)
+        ::JWT::Base64.url_encode(key_part.to_s(BINARY))
       end
 
       class << self
@@ -107,7 +107,7 @@ module JWT
         def decode_open_ssl_bn(jwk_data)
           return nil unless jwk_data
 
-          OpenSSL::BN.new(::Base64.urlsafe_decode64(jwk_data), BINARY)
+          OpenSSL::BN.new(::JWT::Base64.url_decode(jwk_data), BINARY)
         end
       end
     end
