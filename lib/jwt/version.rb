b1b52504a7d7819240f2bc9a50da1a79ed03df2b
Tim Rudat <timrudat@gmail.com>
2022-08-25 21:56:08 +0200

    Bump version to 2.5.0

--- lib/jwt/version.rb
+++ lib/jwt/version.rb
@@ -11,9 +11,9 @@ module JWT
     # major version
     MAJOR = 2
     # minor version
-    MINOR = 4
+    MINOR = 5
     # tiny version
-    TINY  = 1
+    TINY  = 0
     # alpha, beta, etc. tag
     PRE   = nil
 
