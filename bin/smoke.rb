8e6c82824257688938051c0ae22a1588f20221ff
Joakim Antman <antmanj@gmail.com>
2022-11-12 10:38:39 +0200

    Smoke test for built gem

---
+++ bin/smoke.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'jwt'
+
+puts "Running simple encode/decode test for #{::JWT.gem_version}"
+secret = 'secretkeyforsigning'
+token  = ::JWT.encode({ con: 'tent' }, secret, 'HS256')
+::JWT.decode(token, secret, true, algorithm: 'HS256')
